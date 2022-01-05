2d406bdb9f6057e6cd7461ce4cccc76c12401b29
Rob Cole <rob-cole@hotmail.co.uk>
2022-01-05 16:43:16 +0000

    Add code for bullets in game

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,17 +47,18 @@ var config = {
 var player;
 var group;
 var cursors;
+var bullets;
+var speed;
+var stats;
+var lastFired = 0;
 
 var game = new Phaser.Game(config);
 
 function preload (){
   this.load.image('sky','./assets/images/nebula.png');
-  this.load.image('red-roid','./assets/images/red-roid.png');
-  this.load.image('aqua-roid','./assets/images/aqua-roid.png');
-  this.load.image('gold-roid','./assets/images/gold-roid.png');
   this.load.image('roid','./assets/images/grey-roid.png');
-  this.load.image('orange-roid','./assets/images/orange-roid.png');
   this.load.image('ship','./assets/images/ship.png');
+  this.load.image('bullet','./assets/images/bullet.png');
 }
 
 function create(){
@@ -69,6 +70,8 @@ function create(){
 
   cursors = this.input.keyboard.createCursorKeys();
 
+  speed = Phaser.Math.GetSpeed(300,1);
+
   group = this.add.group({
     defaultKey: 'roid',
     maxSize: 80,
@@ -79,39 +82,66 @@ function create(){
     removeCallback: function (roid) {
         console.log('Removed', roid.name);
     }
-});
+  });
 
-// You could also fill the group first:
-// group.createMultiple({
-//     active: false,
-//     key: group.defaultKey,
-//     repeat: group.maxSize - 1
-// });
-
-this.time.addEvent({
+  this.time.addEvent({
     delay: 100,
     loop: true,
     callback: addRoid
-});
+  });
 
+  var Bullet = new Phaser.Class({
+    Extends: Phaser.GameObjects.Image,
+
+    initialize:
+    function Bullet (scene){
+      Phaser.GameObjects.Image.call(this, scene, 0, 0, 'bullet');
+      this.speed = Phaser.Math.GetSpeed(400,1);
+    },
+
+    fire: function (x,y){
+      this.setPosition(x, y - 50);
+      this.setActive(true);
+      this.setVisible(true);
+    },
+
+    update: function (time, delta){
+      this.y -= this.speed * delta;
+
+      if (this.y < -50){
+        this.setActive(false);
+        this.setVisible(false);
+      }
+    }
+  });
+
+  bullets = this.add.group({
+    classType: Bullet,
+    maxSize: 10,
+    runChildUpdate: true
+  });
 }
 
-function update(){
-  if (cursors.left.isDown){
-    player.setVelocityX(-160)
-  }
-  else if (cursors.right.isDown){
-    player.setVelocityX(160)
-  }
-  else {
-    player.setVelocityX(0)
+function update(time, delta){
+  if (cursors.left.isDown)
+    {player.x -= speed * delta;}
+  else if (cursors.right.isDown)
+    {player.x += speed * delta;}
+
+  if(cursors.space.isDown && time > lastFired){
+    var bullet = bullets.get();
+
+    if (bullet){
+      bullet.fire(player.x, player.y);
+      lastFired = time + 50;
+    }
   }
 
   Phaser.Actions.IncY(group.getChildren(), 1);
 
   group.children.iterate(function (roid) {
       if (roid.y > 600) {
-          group.killAndHide(roid);
+        group.killAndHide(roid);
       }
   });
 }
