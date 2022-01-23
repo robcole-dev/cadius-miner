78e3ae46424eaa89eee68cb4a1cb9ab89d59cb40
Rob Cole <rob-cole@hotmail.co.uk>
2022-01-23 16:26:25 +0000

    Add JS for show/hide how to section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
+// Function to display how to play instructions
+
+function show() {
+  let display = document.getElementById("display");
+  if (display.style.display === "none") {
+    display.style.display = "block";
+  } else {
+    display.style.display = "none";
+  }
+}
+
 // Game 
 
 var config = {
