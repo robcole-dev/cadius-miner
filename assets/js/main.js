63ddc5dfdbfcc8a0ade076380eba5c872ac06409
Rob Cole <rob-cole@hotmail.co.uk>
2022-02-06 10:58:13 +0000

    Add new JS file for index page

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+
+// Function to display how to play instructions
+
+function show() {
+    let display = document.getElementById("display");
+    if (display.style.display === "none") {
+      display.style.display = "block";
+    } else {
+      display.style.display = "none";
+    }
+  };
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,5 @@
 /* jshint esversion: 8 */
 
-// Function to display how to play instructions
-
-function show() {
-  let display = document.getElementById("display");
-  if (display.style.display === "none") {
-    display.style.display = "block";
-  } else {
-    display.style.display = "none";
-  }
-};
-
 // Game Configuration
 let config = {
   type: Phaser.AUTO,
