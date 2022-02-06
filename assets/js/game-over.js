165cccfbb1004739bdb421f01167914e5c037fc7
Rob Cole <rob-cole@hotmail.co.uk>
2022-02-06 15:09:31 +0000

    Add code comments

--- assets/js/game-over.js
+++ assets/js/game-over.js
@@ -1,5 +1,6 @@
 /* jshint esversion: 8 */
 
+// New Scene class
 let gameOver = new Phaser.Class({
     Extends: Phaser.Scene,
     initialize: function(){
@@ -7,16 +8,18 @@ let gameOver = new Phaser.Class({
         key: "gameOver",
         })
     },
-
+    // Preload Images for Scene
     preload: function() {
         this.load.image('sky','assets/images/nebula.png');
     },
-
+    // Create the Scene
     create: function() {
+        // define scene as this
         scene = this
         let hscore
+        // define highscore as the players local storage score
         let highscore = localStorage.getItem(hscore)
-
+        // If score is higher than highscore write it to local storage
         if (score > highscore){
             highscore = score
             localStorage.setItem(hscore, highscore)
@@ -24,16 +27,19 @@ let gameOver = new Phaser.Class({
 
         // add background to game
         this.add.image(400,400,'sky');
-
+        // Game over text
         gameOverText = this.add.text(400,200, 'GAME OVER!!!', {fontSize: '50px', fill: '#00ff00'});
         gameOverText.setOrigin(0.5);
+        // Score text
         scoreText = this.add.text(400,250, 'You scored ' + score, {fontSize: '50px', fill: '#00ff00'});
         scoreText.setOrigin(0.5);
+        // Highscore text
         highscoreText = this.add.text(400,300, 'Your Highscore ' + highscore, {fontSize: '50px', fill: '#00ff00'});
         highscoreText.setOrigin(0.5);
+        // retry text
         retryText = this.add.text(400,450, 'Click to try again!', {fontSize: '50px', fill: '#00ff00'});
         retryText.setOrigin(0.5);
-
+        // click to next scene for retry
         this.input.on('pointerdown', function(){
             scene.scene.start('mainMenu');
         });
--- assets/js/main-menu.js
+++ assets/js/main-menu.js
@@ -1,5 +1,6 @@
 /* jshint esversion: 8 */
 
+// Main Menu Class
 let mainMenu = new Phaser.Class ({
     Extends: Phaser.Scene,
     initialize: function mainMenu() {
@@ -7,20 +8,21 @@ let mainMenu = new Phaser.Class ({
             key: "mainMenu",
         });
     },
-    
+    // Preload for all images used in scene
     preload: function() {
         this.load.image('sky','assets/images/nebula.png');
     },
-
+    // Create the scene
     create: function() {
+        // Scene background
         this.add.image(400,400,'sky');
-        
+        // define scene as this
         scene = this;
-
+        // Play text on screen
         playText = this.add.text(350,300,'Play', {fontSize: '50px', fill: '#00ff00'});
 
         playText.setInteractive();
-
+        // Play text on click function move to next scene
         this.input.on('pointerdown', function(){
             scene.scene.start('mainGame');
         });
