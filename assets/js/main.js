7c0bcf012c5a4e3e94b856d93fe14c377ca9a5ba
Rob Cole <rob-cole@hotmail.co.uk>
2022-02-08 12:55:37 +0000

    Add High score message on main page

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,6 +15,14 @@ function show() {
     }
   }
 
+// Previous high score message
+
+if (localStorage.getItem("hscore") > 0) {
+  document.getElementById("prev-hscore").innerHTML = `<p>Your Previous High Score was ${localStorage.getItem("hscore")}! Can you beat it?</p>`
+} else {
+  document.getElementById("prev-hscore").innerHTML = '<p>You currently have no high score!<br>Click Play and try your luck!</p>'
+}
+
 // event listeners
 let infoButton = document.getElementById('show');
 infoButton.addEventListener('click',show);
